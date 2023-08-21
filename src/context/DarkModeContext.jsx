b08c4e4aea27f2df8320b1dc959e2126fbae7f14
yoosaemsol <yoosaemsol@gmail.com>
2023-08-21 18:18:26 +0900

    darkmode: 다크모드 셋팅을 localStorage 로도 관리하기.

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const DarkModeContext = createContext();
 
@@ -8,6 +8,16 @@ export function DarkModeProvider({ children }) {
     setDarkMode(!darkMode);
     updateDarkMode(!darkMode);
   };
+
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
 
   return (
     <DarkModeContext.Provider
@@ -21,16 +31,18 @@ export function DarkModeProvider({ children }) {
   );
 }
 
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+}
+
 export const useDarkMode = () => useContext(DarkModeContext);
 
 // hook으로 내보내지 않는다면, 사용부에서
 // const { darkMode, toggleDarkMode } = useContext(DarkModeContext)
 // 를 해주어야 한다.
-
-function updateDarkMode(darkMode) {
-  if (darkMode) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
-}
