3ddd31f2bafc49b408adb518417a6331aaaa2e3d
yoosaemsol <yoosaemsol@gmail.com>
2023-08-21 17:45:58 +0900

    style: Todo 아이템 스타일링.

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
+import styles from './Todo.module.css';
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { text, status, id } = todo;
@@ -16,17 +17,23 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   const handleDelete = () => onDelete(todo);
 
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type="checkbox"
         id={id}
         checked={status === 'completed'}
         onChange={handleChange}
       />
-      <label htmlFor={id}>{text}</label>
-      <button onClick={handleDelete}>
-        <FaTrashAlt />
-      </button>
+      <label htmlFor={id} className={styles.text}>
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button className={styles.button} onClick={handleDelete}>
+          {/* <FaTrashAlt /> */}
+          🗑️
+        </button>
+      </span>
     </li>
   );
 }
